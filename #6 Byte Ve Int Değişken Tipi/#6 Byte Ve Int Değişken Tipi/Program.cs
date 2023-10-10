147770e4649e4a14ea43e8d826d0dfb25d8920e7
Enes Berke Karaoğlan <115350792+Enes-berke@users.noreply.github.com>
2023-10-10 14:24:51 +0300

    C-Sharp-Programming-Turkish

---
+++ #6 Byte Ve Int Değişken Tipi/#6 Byte Ve Int Değişken Tipi/Program.cs
@@ -0,0 +1,16 @@
+﻿byte deger1 = 10;
+
+int deger2 = 100;
+
+byte byteMinValue = byte.MinValue(); //  0
+Console.WriteLine(deger1);
+
+byte byteMaxValue = byte.MaxValue(); // 255
+Console.WriteLine(deger2);
+
+int intMinValue = int.MinValue(); // -2147483648
+Console.WriteLine(intMinValue);
+
+int intMaxValue = int.MaxValue(); // 2147483647
+Console.WriteLine(intMaxValue);
+    
